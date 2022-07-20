825ed95f588e141d5b693e7c92b55b0e9c35f913
arielpodesta <arielpodesta@gmail.com>
2022-07-20 00:24:55 -0300

    Updates

---
+++ precache-manifest.31828ee89b58252c203ab2a96a970c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9396b856f6523f4972199e4c3d6b4fa5",
+    "url": "/arielpodesta/index.html"
+  },
+  {
+    "revision": "51954a62c3cb600df7f6",
+    "url": "/arielpodesta/static/css/main.02439012.chunk.css"
+  },
+  {
+    "revision": "a3f15fec2fbd90a7abd2",
+    "url": "/arielpodesta/static/js/2.e4fd4e8e.chunk.js"
+  },
+  {
+    "revision": "51954a62c3cb600df7f6",
+    "url": "/arielpodesta/static/js/main.50d98242.chunk.js"
+  },
+  {
+    "revision": "3fee033db9aa6b1ae7f2",
+    "url": "/arielpodesta/static/js/runtime-main.2d07b792.js"
+  }
+]);
