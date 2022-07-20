b56d356f1b8cba624dddac0f8742b964b4b038d8
arielpodesta <arielpodesta@gmail.com>
2022-07-19 23:23:35 -0300

    .

---
+++ build/precache-manifest.121b1979aa39105e2ffcab622fc6ffb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f15eee09f50ebfa6420b142a303683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f39de27c14b135f07f2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "da9b81792bb6de087a0f",
+    "url": "/static/js/2.f0dba5b6.chunk.js"
+  },
+  {
+    "revision": "3f39de27c14b135f07f2",
+    "url": "/static/js/main.c609888d.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
